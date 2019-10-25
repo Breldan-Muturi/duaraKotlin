0284f8ab2e39061d83475eb4d3a0400580df33ac
Turi-byte <breldanmuturi001@gmail.com>
2019-10-25 21:01:20 +0300

    Add user login with realtime Database

---
+++ app/src/main/java/turi/duara/duarakotlin/CreateAccountActivity.kt
@@ -0,0 +1,90 @@
+package turi.duara.duarakotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_create_account.*
+
+class CreateAccountActivity : AppCompatActivity() {
+    var mAuth: FirebaseAuth? = null
+    var mDatabase: DatabaseReference? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+        mAuth = FirebaseAuth.getInstance()
+        accountCreateActBtn.setOnClickListener {
+            var email = accountEmailEt.text.toString().trim()
+            var password = accountPasswordEt.text.toString().trim()
+            var displayName = accountDisplayNameEt.text.toString().trim()
+
+            if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)
+                || !TextUtils.isEmpty(displayName)
+            ) {
+                createAccount(email, password, displayName)
+            } else {
+                Toast.makeText(this, "Please fill out the fields", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+        }
+
+    }
+
+    fun createAccount(email: String, password: String, displayName: String) {
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task: Task<AuthResult> ->
+
+                if (task.isSuccessful) {
+                    var currentUser = mAuth!!.currentUser
+                    var userId = currentUser!!.uid
+
+                    mDatabase = FirebaseDatabase.getInstance().reference
+                        .child("Users").child(userId)
+
+                    /*
+                        Users
+                            - 77rkdkekkdkd
+                               - Paulo
+                               - "hello there"
+                               - "image url.."
+                     */
+
+                    var userObject = HashMap<String, String>()
+                    userObject.put("display_name", displayName)
+                    userObject.put("status", "Hello there....")
+                    userObject.put("image", "default")
+                    userObject.put("thumb_image", "default")
+
+                    mDatabase!!.setValue(userObject).addOnCompleteListener { task: Task<Void> ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "User Created!", Toast.LENGTH_LONG).show()
+
+                        } else {
+
+                            Toast.makeText(this, "User Not Created!", Toast.LENGTH_LONG)
+                                .show()
+
+                        }
+                    }
+
+
+                } else {
+
+                }
+
+
+            }
+
+
+    }
+
+}
--- app/src/main/java/turi/duara/duarakotlin/MainActivity.kt
+++ app/src/main/java/turi/duara/duarakotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package turi.duara.duarakotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,49 +13,17 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var mAuth: FirebaseAuth? = null
-    private var currentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val firebaseDatabase = FirebaseDatabase.getInstance()
-        val databaseRef = firebaseDatabase.getReference("messages").push()
-        mAuth = FirebaseAuth.getInstance()
-//        Sign existing users in
-        mAuth!!.signInWithEmailAndPassword("breldanmuturi001@outlook.com","Sunrise@1997")
-            .addOnCompleteListener{
-                task: Task<AuthResult> ->
-                if(task.isSuccessful){
-//                    Sign in was successful
-                    Toast.makeText(this,"Signed In Successful", Toast.LENGTH_LONG).show()
-                } else {
-//                    Not successful
-                    Toast.makeText(this,"Sign In Unsuccessful", Toast.LENGTH_LONG).show()
-
-                }
-            }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        currentUser = mAuth!!.currentUser
-//        Call a function that will update the UI with current user
-        if(currentUser != null){
-            Toast.makeText( this, "User is logged in", Toast.LENGTH_LONG).show()
-        } else {
-            Toast.makeText( this, "User is logged out", Toast.LENGTH_LONG).show()
+        createActButton.setOnClickListener{
+            startActivity(Intent(this,CreateAccountActivity::class.java))
         }
     }
 
-    data class Employee(
-        val name: String,
-        val position: String,
-        val homeAddress: String,
-        val age: Int
-    )
 }
