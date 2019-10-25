7ba21fe9b0c8c86d51f7ca3e9fe617d0a62f75b9
Turi-byte <breldanmuturi001@gmail.com>
2019-10-25 17:23:12 +0300

    Add chat login activity

--- app/src/main/java/turi/duara/duarakotlin/MainActivity.kt
+++ app/src/main/java/turi/duara/duarakotlin/MainActivity.kt
@@ -3,6 +3,11 @@ package turi.duara.duarakotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -10,24 +15,46 @@ import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
+    private var mAuth: FirebaseAuth? = null
+    private var currentUser: FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val firebaseDatabase = FirebaseDatabase.getInstance()
-        val databaseRef = firebaseDatabase.getReference("messages")
+        val databaseRef = firebaseDatabase.getReference("messages").push()
+        mAuth = FirebaseAuth.getInstance()
+//        Sign existing users in
+        mAuth!!.signInWithEmailAndPassword("breldanmuturi001@outlook.com","Sunrise@1997")
+            .addOnCompleteListener{
+                task: Task<AuthResult> ->
+                if(task.isSuccessful){
+//                    Sign in was successful
+                    Toast.makeText(this,"Signed In Successful", Toast.LENGTH_LONG).show()
+                } else {
+//                    Not successful
+                    Toast.makeText(this,"Sign In Unsuccessful", Toast.LENGTH_LONG).show()
 
-        databaseRef.setValue("Hello I love android and kotlin!")
-        //Read frm our DB
-        databaseRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val value = dataSnapshot.value
-                Log.d("VALUE", value.toString())
+                }
             }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.d("ERROR", error!!.message)
-            }
-        })
     }
+
+    override fun onStart() {
+        super.onStart()
+        currentUser = mAuth!!.currentUser
+//        Call a function that will update the UI with current user
+        if(currentUser != null){
+            Toast.makeText( this, "User is logged in", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText( this, "User is logged out", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    data class Employee(
+        val name: String,
+        val position: String,
+        val homeAddress: String,
+        val age: Int
+    )
 }
